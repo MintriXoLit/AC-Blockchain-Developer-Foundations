36ad12862d47e4f783977bfb73a3ba28e93fe5d8
Nguyen Minh Tri <lam.tri2k1@gmail.com>
2025-09-03 22:17:58 +0700

    Update solution.ts and lockfiles

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,4 +1,5 @@
 
+import {createHash} from "crypto";
 export type Block = {
   index: number;
   timestamp: string;
@@ -8,8 +9,12 @@ export type Block = {
 };
 
 // ✍️ TODO: Viết hàm tại đây
+function calculateHash(index: Number, timestamp: string, transactions: any[], previous_hash: string): string {
+    const value = index + timestamp + JSON.stringify(transactions) + previous_hash;
+    return createHash('sha256').update(value).digest('hex');
+}
 export function isValidBlock(block: Block): boolean {
-  if( SHA256(SHA256(block.index.toString()+block.timestamp+JSON.stringify(block.transactions)+block.previous_hash)).toString()===block.current_hash)
+  if( calculateHash(block.index,block.timestamp,block.transactions,block.previous_hash)===block.current_hash)
   {return true;}
   return false; // Chỉnh lại logic
 }
