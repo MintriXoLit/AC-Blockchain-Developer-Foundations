12ea22c140bd6a99f20fd659a270a5f5727edbc6
MintriXoLit <lam.tri2k1@gmail.com>
2025-09-03 21:19:12 +0700

    Update solution.ts

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -9,5 +9,7 @@ export type Block = {
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
+  if( SHA256(SHA256(block.index.toString()+block.timestamp+JSON.stringify(block.transactions)+block.previous_hash)).toString()===block.current_hash)
+  {return true;}
   return false; // Chỉnh lại logic
 }
